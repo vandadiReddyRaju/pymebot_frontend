7aab6e493ac7736a97446b694a357c13af186b01
vandadiReddyRaju <140302379+vandadiReddyRaju@users.noreply.github.com>
2025-03-13 02:11:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
     try {
       setIsLoading(true);
-      const response = await fetch('/api/submit', {
+      const response = await fetch('http://127.0.0.1:8000/api/submit', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ questionId, query, code }),
